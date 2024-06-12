4fa8ec938042f669378bd6c15f3c2a76151a9a98
Cristian Loera <loera_73@hotmail.com>
2024-06-12 16:11:52 -0600

    Add the first files i'm going to work with

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO department (name)
+VALUES  ('Engineering'),
+        ('Finance'),
+        ('Legal'),
+        ('Sales');
+
+INSERT INTO role (title, department_id, salary)
+VALUES  ('Sales Lead', 'Sales', 100000),
+        ('Salesperson', 'Sales', 80000),
+        ('Lead Engineer', 'Engineering', 150000),
+        ('Software Engineer', 'Engineering', 120000),
+        ('Account Manager', 'Finance', 160000),
+        ('Accountant', 'Finance', 125000),
+        ('Legal Team Lead', 'Legal', 250000),
+        ('Lawyer', 'Legal', 190000);
+
